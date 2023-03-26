fe965087ba39771b039e7fe146f720794f0a5695
jojobutercup <jojobutercup@gmail.com>
2023-03-26 14:36:41 -0700

    commit

---
+++ Variable_and_simple_data_type/adding_comments.py
@@ -0,0 +1,7 @@
+# my name is osariemen 
+#this code assigns values to variables, combines the variable names in te string 
+#and prints out a full, including a newline and a tab space to the message
+first_name = "peterson"
+last_name = "jeff"
+famous_person = f"{first_name} {last_name}"
+print(f'{famous_person.title()} said \n\t"Real men are not bitter for being rejected, \nthey do everything they can, \nto kneel before the eternal image of the feminine"')
---
+++ Variable_and_simple_data_type/number_eight.py
@@ -0,0 +1,21 @@
+#Write addition, subtraction, multiplication, and division
+#operations that each result in the number 8. Be sure to enclose your operations operations that each result in the number 8. Be sure to enclose your operations
+a = 5
+b = 2
+c = 1
+print(a + b + c)
+
+a = 4
+b = 2
+c = 2
+print(a * b)
+
+a = 8
+b = 0
+c = 0
+print(a / b)
+
+a = 8
+b = 0
+c = 0
+print(a - b)
