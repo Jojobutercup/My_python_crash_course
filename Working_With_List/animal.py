c5adfbe277828728130489a0418a305481290c6e
jojobutercup <jojobutercup@gmail.com>
2023-04-03 03:51:23 -0700

    commit

---
+++ Working_With_List/animal.py
@@ -0,0 +1,8 @@
+#Modify your program to print a statement about each animal, such as A dog would make a great pet
+animals = ['fish', 'cat', 'lizard']
+for animal in animals:
+    print(f"{animal.title()}, is a great choice. depemding on your personality")
+    print(f"{animal.title()}, these animaks dont like to be petted , but any of this animals will make a great pet\n")
+
+
+    
---
+++ Working_With_List/pizza.py
@@ -0,0 +1,5 @@
+#Modify your for loop to print a sentence using the name of the pizza
+pizzas = ['pepperoni', 'meatpie', 'suya']
+for pizza in pizzas:
+    print(f"{pizza.title()}, is one of my favourite pizza flavor\n")
+print("I really like pizza")
