f353c64ca162889ae04a6912c8628c62ce92a973
jojobutercup <jojobutercup@gmail.com>
2023-04-04 11:40:23 -0700

    commit

---
+++ Working_With_List/more_loops.py
@@ -0,0 +1,9 @@
+#All versions of foods.py in this section have avoided using for loops when printing to save space. Choose a version of foods.py, and write two for loops to print each list of foods.
+my_foods = ['pizza', 'falafel', 'carrot cake']
+print("My best foods are:")
+for my_food in my_foods:
+    print(my_food)
+my_friends =  ['pizza', 'falafel', 'carrot cake', 'ice cream']
+print("these are my best friend's food:")
+for my_friend in my_friends:
+    print(my_friend)
