0dacf13560f2fac4c8083d0d93c2396d5c0e7f06
jojobutercup <jojobutercup@gmail.com>
2023-04-08 10:27:59 -0700

    commit

---
+++ CHAPTER_3/code_review.py
@@ -0,0 +1,10 @@
+# A buffet-style restaurant offers only five basic foods. Think of five simple foods, and store them in a tuple Use a for loop to print each food the restaurant offers.Try to modify one of the items, and make sure that Python rejects the change The restaurant changes its menu, replacing two of the items with differen foods. Add a line that rewrites the tuple, and then use a for loop to printfoods. Add a line that rewrites the tuple, and then use a for loop to print
+menus = ('salad', 'rice', 'snacks', 'beef','garri')
+print("we offer these five basic foods:")
+for menu in menus:
+    print(menu)
+
+menus = ('salad', 'rice', 'snacks', 'beef','garri', 'spaghetti', 'beans')
+print("\n, This is the modified list;")
+for menu in menus:
+    print(menu
