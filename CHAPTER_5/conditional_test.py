7e4f30b95dda48a1692c422cab104142b400f59d
jojobutercup <jojobutercup@gmail.com>
2023-04-10 14:28:03 -0700

    commit

--- CHAPTER_5/conditional_test.py
+++ CHAPTER_5/conditional_test.py
@@ -6,31 +6,42 @@ else:
     print("name != 'michel'")
 #Create at least ten tests. Have at least five tests evaluate to True and another five tests evaluate to False.
 car = 'sabaru'
+print("is car == 'sabaru'? I predict True")
 print(car == 'sabaru')
 
 car = 'bmw'
+print("is car == 'bmw'? I predict True")
 print(car == 'bmw')
 
 car = 'toyota'
+print("is car == 'toyota'? I predict True")
 print(car == 'toyota')
 
 car = 'audi'
+print("is car == 'audi'? I predict True")
 print(car == 'audi')
 
 car = 'mercedes'
+print("is car == 'mercedes'? I predict True")
 print(car == 'mercedes')
 
 car = 'bmw'
+print("is car == 'mercedes'? I predict False")
 print(car == 'mercedes')
 
 car = 'toyota'
+print("is car == 'audi'? I predict False")
 print(car == 'audi')
 
 car = 'sabaru'
-print(car == 'bnw')
+print("is car == 'bmw'? I predict False")
+print(car == 'bmw')
 
 car = 'mercedes'
+print("is car == 'mercedes'? I predict False")
 print(car == 'toyota')
 
 car = 'audi'
+print("is car == 'sabaru'? I predict False")
 print(car == 'subaru')
+
