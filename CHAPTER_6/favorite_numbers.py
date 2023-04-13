e51c04f9ae76a89f223f6caf011377dc6ff97d81
jojobutercup <jojobutercup@gmail.com>
2023-04-13 12:00:29 -0700

    commit

--- CHAPTER_6/favorite_numbers.py
+++ CHAPTER_6/favorite_numbers.py
@@ -1,11 +1,11 @@
 #Use a dictionary to store people’s favorite numbers. Think of five names, and use them as keys in your dictionary. Think of a favorite number for each person, and store each as a value in your dictionary. Print each person’s name and their favorite number. For even more fun, poll a few friends and get some actual data for your program
 favorite_number = {
-        'oluwajuwon': 7, 
-        'osas': 10,
-        'timi': 17,
-        'dami': 7,
-        'sharon': 8,
-        }
+    'oluwajuwon': 7, 
+    'osas': 10,
+    'timi': 17,
+    'dami': 7,
+    'sharon': 8,
+    }
 J_number = favorite_number['oluwajuwon']
 O_number = favorite_number['osas']
 T_number = favorite_number['timi']
--- CHAPTER_6/glossary.py
+++ CHAPTER_6/glossary.py
@@ -1,11 +1,11 @@
 #Python dictionary can be used to model an actual dictionary. However, to avoid confusion, let’s call it a glossary
 #Think of five programming words you’ve learned about in the previous chapters. Use these words as the keys in your glossary, and store their meanings as values
 Glossary = {
-        'variable': 'is a name assigned to a value', 
-        'list': 'a group of items in a square bracket', 
-        'tuple': 'a list with permanent values', 
-        'if statement': 'is used to test for conditions, which results to either True or False test', 
-        'for loop': 'goes through items in a list'
+    'variable': 'is a name assigned to a value', 
+    'list': 'a group of items in a square bracket', 
+    'tuple': 'a list with permanent values', 
+    'if statement': 'is used to test for conditions, which results to either True or False test', 
+    'for loop': 'goes through items in a list'
         }
 #Print each word and its meaning as neatly formatted output. You might print the word followed by a colon and then its meaning, or print the word on one line and then print its meaning indented on a second line. Use the newline character (\n) to insert a blank line between each word-meaning pair in your output
 print(f"Variable: \n{Glossary['variable']}" )
